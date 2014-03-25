869bb163c24bb6ae56a23f468a321e5f1ab52ebd
Vikyath Harekal <vikyath.harekal@infibeam.net>
2014-03-25 11:15:52 +0530

    added docs

--- temp.py
+++ temp.py
@@ -1,6 +1,5 @@
 import snaide
-pages = snaide.get_all_pages()
-users = snaide.get_all_users()
-print pages
-print "<br>"
-print users
+print "hi"
+print snaide.get_all_pages(),
+print snaide.get_all_users()
+print "bye"
