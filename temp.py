b5634eac7e7e196b0146a6ff93fa34ae08b674ba
Vikyath Harekal <vikyath.harekal@infibeam.net>
2014-04-08 12:17:35 +0530

    removed unwanted shit

--- temp.py
+++ temp.py
@@ -1,6 +1,3 @@
 import snaide
 
 print snaide.get_all_users()
-for page in snaide.get_all_pages():
-	print page["name"]
-g = snaide.get_all_pages()
--- vikyath.py
+++ vikyath.py
@@ -1,4 +1,3 @@
 import snaide
-for page in snaide.get_all_pages()
+for page in snaide.get_all_pages():
 	print page["name"]
-print snaide.get_all_pages()
